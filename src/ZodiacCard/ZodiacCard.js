77ffbf0438b97370181290a0334e4ac66091606d
Khayman King <khaymanak@gmail.com>
2022-04-25 15:41:16 -0700

    now renders images

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -3,7 +3,7 @@ import './ZodiacCard.css';
 export default function ZodiacCard({ zodiac }) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
   return (
-    <div className='zodiac-card' key={zodiac.name}>{zodiac.dates} <img src= {zodiac.image}/>
+    <div className='zodiac-card' key={zodiac.name}>{zodiac.dates} <img src= {`/images/${zodiac.name}.png`}/>
     </div>
   );
 }
