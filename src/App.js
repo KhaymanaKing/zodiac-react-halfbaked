aa02afc52ff5ba2e78cf580662901c5e99ae73c8
Khayman King <khaymanak@gmail.com>
2022-04-25 13:02:11 -0700

    started imports on App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 {/* import the zodiac array */}
+import { zodiac } from './data.js';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Main from './Main.js';
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
+      <Header/>
+      <Main></Main>
+      
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
