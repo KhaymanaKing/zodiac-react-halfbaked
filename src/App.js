25e24589ab2c0f4f8986efc8eb5df19bf75e63b7
Khayman King <khaymanak@gmail.com>
2022-04-25 15:18:32 -0700

    zodiacs are now showing without images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 {/* import the zodiac array */}
-import { zodiac } from './data.js';
-import Header from './Header.js';
-import Footer from './Footer.js';
-import Main from './Main.js';
+import { zodiacs } from './data.js';
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+import Main from './Main/Main.js';
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
-      <Header name=''/>
-      <Main />
-      <Footer dates=''/>
-
+      <Header name='Khayman'/>
+      <Main zodiacs={zodiacs} />
+      <Footer year='2022'/>
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
